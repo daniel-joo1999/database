9046a8cbafd9a47bd23ecdb1579232e4f0c8e26a
Jun Song Park <jp6ax@virginia.edu>
2021-05-02 17:59:06 -0400

    Final Visited

--- visitedPlaces.php
+++ visitedPlaces.php
@@ -1,68 +1,129 @@
 <?php
-session_start();
-include_once("./library.php");
-$con = new mysqli($SERVER, $USERNAME, $PASSWORD, $DATABASE);
-$lid = $_GET['lid'];
-$title = $_GET['lname'];
-if (mysqli_connect_errno())
- {
- echo "Failed to connect to MySQL: " . mysqli_connect_error();
- }
- $query="SELECT lname FROM location 
-        WHERE exists (select 1 from visited where uid='".$_SESSION['id']."'";
 
+session_start();
+
+include_once("./library.php");
+
+$con = new mysqli($SERVER, $USERNAME, $PASSWORD, $DATABASE);
+
+if (mysqli_connect_errno())
+
+ {
+
+ echo "Failed to connect to MySQL: " . mysqli_connect_error();
+
+ }
+ $result = mysqli_query($con,"SELECT lname, dateVisited FROM location NATURAL JOIN visited WHERE uid = $_SESSION[id]
+ ");
+// $result = mysqli_query($con, "Select ")
 ?>
+
+
 
 <!DOCTYPE html>
+
 <html>
+
  <head>
+
  <title>Visited</title>
+
  <style>
+
      table {
+
     font-family: arial, sans-serif;
+
     border-collapse: collapse;
+
     width: 100%;
+
     }
+
+
 
     td, th {
+
         border: 1px solid #dddddd;
+
         text-align: left;
+
         padding: 8px;
 
+
+
     }
 
+
+
     tr:nth-child(even) {
+
         background-color: white;
+
     }
+
  </style>
+
  
+
  </head>
+
 <body>
+
 <?php
+
 if (mysqli_num_rows($result) > 0) {
+
 ?>
+
   <table>
+
   
+
   <tr>
+
     <td>Location Visited</td>
+    <td>Date Visited</td>
+
   </tr>
+
 <?php
+
 $i=0;
+
 while($row = mysqli_fetch_array($result)) {
+
 ?>
+
 <tr>
+
     <td><?php echo $row["lname"]; ?></td>
+    <td><?php echo $row["dateVisited"]; ?></td>
+
 </tr>
+
 <?php
+
 $i++;
+
 }
+
 ?>
+
 </table>
+
  <?php
+
 }
+
 else{
+
     echo "No Visited Locations";
+
 }
+
 ?>
+
  </body>
+
 </html>
